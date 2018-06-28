58e954c5c5bf30309e3c7ff961b2f93072a1df48
Jerônimo G. Grandi <jeronimograndi@gmail.com>
2018-06-28 19:58:30 -0300

    Syncing gravity. Bug because of SyncTransform being called for both clients even if they are not manipulating objects

--- Assets/Scripts/HandleTransformations.cs
+++ Assets/Scripts/HandleTransformations.cs
@@ -7,24 +7,30 @@ public class HandleTransformations : NetworkBehaviour {
 
     public GameObject allObjects;
 
-    //[Command]
-    //void CmdSyncTransform(int index, Transform objTransform)
-    //{
-    //    var g = ObjectManager.Get(index);
-    //    g.transform.position = objTransform.position;
-    //    g.transform.rotation = objTransform.rotation;
-    //    RpcSyncTransform(index, objTransform);
-    //    Debug.Log("Server");
-    //}
+    [Command]
+    void CmdSyncTransform(int index, Vector3 objPos, Quaternion objRot, bool gravity)
+    {
+        var g = ObjectManager.Get(index);
+        g.transform.position = objPos;
+        g.transform.rotation = objRot;
+        g.transform.GetComponent<Rigidbody>().useGravity = gravity;
 
-    //[ClientRpc]
-    //void RpcSyncTransform(int index, Transform objTransform)
-    //{
-    //    var g = ObjectManager.Get(index);
-    //    g.transform.position = objTransform.position;
-    //    g.transform.rotation = objTransform.rotation;
-    //    Debug.Log("Client");
-    //}
+
+        //g.transform.position = Vector3.Lerp(g.transform.position, objPos, 0.2f);
+        //g.transform.rotation = Quaternion.Slerp(g.transform.rotation, objRot, 0.2f);
+        RpcSyncTransform(index, g.transform.position, g.transform.rotation,gravity);
+        //Debug.Log("Server");
+    }
+
+    [ClientRpc]
+    void RpcSyncTransform(int index, Vector3 objPos, Quaternion objRot, bool gravity)
+    {
+        var g = ObjectManager.Get(index);
+        g.transform.GetComponent<Rigidbody>().useGravity = gravity;
+        g.transform.position = objPos;
+        g.transform.rotation = objRot;
+        //Debug.Log("Client");
+    }
 
 
     // Use this for initialization
@@ -38,11 +44,11 @@ public class HandleTransformations : NetworkBehaviour {
 	void Update () {
         if (!isLocalPlayer) return;
 
-        Debug.Log("AAA");
+        
         for(int i=0; i < allObjects.transform.childCount; i++)
         {
             var objTransform = allObjects.transform.GetChild(i);
-            //CmdSyncTransform(i, objTransform );
+            CmdSyncTransform(i, objTransform.position, objTransform.rotation, objTransform.GetComponent<Rigidbody>().useGravity);
         }
 
 
