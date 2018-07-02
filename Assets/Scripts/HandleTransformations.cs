bacf1dddb221db53c90465305e50f564bfc1f6f0
Jerônimo G. Grandi <jeronimograndi@gmail.com>
2018-07-02 19:22:57 -0300

    Added some modification.. not syncing well yet

--- Assets/Scripts/HandleTransformations.cs
+++ Assets/Scripts/HandleTransformations.cs
@@ -16,9 +16,9 @@ public class HandleTransformations : NetworkBehaviour {
         //g.transform.GetComponent<Rigidbody>().useGravity = gravity;
 
 
-        //g.transform.position = Vector3.Lerp(g.transform.position, objPos, 0.2f);
-        //g.transform.rotation = Quaternion.Slerp(g.transform.rotation, objRot, 0.2f);
-        RpcSyncTransform(index, objPos, objRot,gravity);
+        //g.transform.position = Vector3.Lerp(g.transform.position, objPos, 0.02f);
+        //g.transform.rotation = Quaternion.Slerp(g.transform.rotation, objRot, 0.02f);
+        RpcSyncTransform(index, objPos, objRot, gravity);
         //Debug.Log("Server");
     }
 
@@ -26,13 +26,30 @@ public class HandleTransformations : NetworkBehaviour {
     void RpcSyncTransform(int index, Vector3 objPos, Quaternion objRot, bool gravity)
     {
         var g = ObjectManager.Get(index);
-        g.transform.GetComponent<Rigidbody>().useGravity = gravity;
+        var selected = ObjectManager.GetSelected();
 
-        g.transform.position = Vector3.Lerp(g.transform.position, objPos, 0.02f);
-        g.transform.rotation = Quaternion.Slerp(g.transform.rotation, objRot, 0.02f);
-        //g.transform.position = objPos;
-        //g.transform.rotation = objRot;
-        //Debug.Log("Client");
+        if(selected != null && g.name == selected.name)
+        {
+            g.transform.position = Vector3.Lerp(g.transform.position, objPos, 0.02f);
+            g.transform.rotation = Quaternion.Lerp(g.transform.rotation, objRot, 0.02f);
+
+        }
+        else
+        {
+            g.transform.position = Vector3.Lerp(g.transform.position, objPos, 0.1f);
+            g.transform.rotation = Quaternion.Lerp(g.transform.rotation, objRot, 0.1f);
+
+        }
+
+
+
+            //g.transform.GetComponent<Rigidbody>().useGravity = gravity;
+            //g.transform.position = objPos;
+            //g.transform.rotation = objRot;
+            //Debug.Log("Client");
+
+       // }
+
     }
 
 
@@ -44,16 +61,16 @@ public class HandleTransformations : NetworkBehaviour {
 
 
     // Update is called once per frame
-    void Update () {
+    void FixedUpdate () {
         if (!isLocalPlayer) return;
+        if (ObjectManager.GetSelected() == null) return; // if localplayer is not selecting an object
 
         for (int i = 0; i < allObjects.transform.childCount; i++)
         {
             var objTransform = allObjects.transform.GetChild(i);
-            if (ObjectManager.GetSelected() == null) return;
             
             if (objTransform.name == ObjectManager.GetSelected().name)
-                CmdSyncTransform(i, objTransform.position, objTransform.rotation, objTransform.GetComponent<Rigidbody>().useGravity);
+            CmdSyncTransform(i, objTransform.position, objTransform.rotation, objTransform.GetComponent<Rigidbody>().useGravity);
         }
 
 
