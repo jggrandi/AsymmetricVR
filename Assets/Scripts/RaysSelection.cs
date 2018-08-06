e6a817602451a7191c988431775f3daab93832d5
Jerônimo G. Grandi <jeronimograndi@gmail.com>
2018-08-06 13:48:38 -0300

    Adde moverotate icon. Changed the player prefab in order to add all icons necessary. The logic is improved but still only one icon is being shown

--- Assets/Scripts/RaysSelection.cs
+++ Assets/Scripts/RaysSelection.cs
@@ -41,11 +41,11 @@ public class RaysSelection : NetworkBehaviour {
             var head = player.transform.GetChild(0); // if the order changes in the prefab, it is necessary to update these indexes
             var leftController = player.transform.GetChild(1);
             var rightController = player.transform.GetChild(2);
-            var iconMoveLeft = leftController.transform.GetChild(2); // it is the thrird because the other 2 are the controllers (a sphere and a cube)
-            var iconMoveRight = rightController.transform.GetChild(2); // it is the thrird because the other 2 are the controllers (a sphere and a cube)
+            var iconsLeftHand = leftController.transform.GetChild(2); // it is the thrird because the other 2 are the controllers (a sphere and a cube)
+            var iconsRightHand = rightController.transform.GetChild(2); // it is the thrird because the other 2 are the controllers (a sphere and a cube)
 
-            iconMoveLeft.gameObject.SetActive(false); // disable the icon. Only show when an action is performed.
-            iconMoveRight.gameObject.SetActive(false); // disable the icon. Only show when an action is performed.
+            DisableIcons(iconsLeftHand); // disable all icons. Only show when an action is performed.
+            DisableIcons(iconsRightHand); // disable all icons. Only show when an action is performed.
 
             if (player.GetComponent<NetworkIdentity>().isLocalPlayer)
                 color = blueColor;
@@ -53,13 +53,13 @@ public class RaysSelection : NetworkBehaviour {
             if (buttonSync.leftInteractionButtonPressed)
             {
                 if (!player.GetComponent<NetworkIdentity>().isLocalPlayer) // add icons only for other player's actions
-                    AddIcon(iconMoveLeft, leftController, selected);
+                    AddIcon(iconsLeftHand.transform.GetChild(0), leftController, selected);
                 AddLine(leftController.transform.position, ObjectManager.Get(selected).transform.position, color);
             }
             if (buttonSync.rightInteractionButtonPressed)
             {
                 if (!player.GetComponent<NetworkIdentity>().isLocalPlayer) // add icons only for other player's actions
-                    AddIcon(iconMoveRight, rightController, selected);
+                    AddIcon(iconsRightHand.transform.GetChild(0), rightController, selected);
                 AddLine(rightController.transform.position, ObjectManager.Get(selected).transform.position, color);
             }
 
@@ -91,9 +91,13 @@ public class RaysSelection : NetworkBehaviour {
     void AddIcon(Transform icon, Transform controller, int indexObjSelected)
     {
         icon.gameObject.SetActive(true);
-        icon.position = controller.transform.position * 0.3f + ObjectManager.Get(indexObjSelected).transform.position * 0.7f;
+        var obj = ObjectManager.Get(indexObjSelected);
+        //var pos = obj.GetComponent<Renderer>().bounds.extents.sqrMagnitude;
+        //Debug.Log(pos);
+        icon.position = controller.transform.position * 0.3f + obj.transform.position * 0.7f;
+        //icon.position = (controller.transform.position * 0.3f) + ((obj.transform.position - new Vector3(pos, pos, pos)) * 0.7f);
         icon.rotation = Quaternion.LookRotation(new Vector3(0, 1, 0), (Camera.main.transform.position - icon.position).normalized);
-        icon.localScale = new Vector3(0.005f, 0.005f, 0.005f);
+        icon.localScale = new Vector3(0.01f, 0.01f, 0.01f);
     }
 
     void AddLine(Vector3 a, Vector3 b, Color c)
@@ -120,6 +124,12 @@ public class RaysSelection : NetworkBehaviour {
             line.transform.position = new Vector3(500, 500, 500);
         }
         linesUsed = 0;
+    }
+
+    void DisableIcons(Transform handIcons)
+    {
+        for (int i = 0; i < handIcons.transform.childCount; i++)
+            handIcons.transform.GetChild(i).gameObject.SetActive(false);
     }
 
 }
