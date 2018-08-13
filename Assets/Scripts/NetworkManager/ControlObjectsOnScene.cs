9552dd726e16ffd817bd2089ab8e5c742ac47df1
Jerônimo G. Grandi <jeronimograndi@gmail.com>
2018-08-13 19:26:53 -0300

    Controlling what will be enabled based on the player spawned (vr or ar)

---
+++ Assets/Scripts/NetworkManager/ControlObjectsOnScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ControlObjectsOnScene : NetworkBehaviour {
+    public GameObject VRPlayer;
+
+    GameObject netMan;
+    // Use this for initialization
+    void Start () {
+        netMan = GameObject.Find("NetworkManager");
+        var refMyNetMan = netMan.GetComponent<MyNetworkManager>();
+        if (refMyNetMan.curPlayer == 0)
+        {
+            Debug.Log("Activating VR Stuff");
+            VRPlayer.SetActive(true);
+        }
+        else if (refMyNetMan.curPlayer == 1)
+            Debug.Log("Activating AR Stuff");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
