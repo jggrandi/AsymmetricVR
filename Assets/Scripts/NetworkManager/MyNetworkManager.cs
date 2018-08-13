d6332d803c11cb4700566fa5f0aaf693b5826f47
Jerônimo G. Grandi <jeronimograndi@gmail.com>
2018-08-13 18:02:09 -0300

    First steps on integration

---
+++ Assets/Scripts/NetworkManager/MyNetworkManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+
+public class MyNetworkManager : NetworkManager
+{
+
+    public int curPlayer;
+
+
+    //Called on client when connect
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+
+        // Create message to set the player
+        IntegerMessage msg = new IntegerMessage(curPlayer);
+
+        // Call Add player and pass the message
+        ClientScene.AddPlayer(conn, 0, msg);
+
+    }
+
+    // Server
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId, NetworkReader extraMessageReader)
+    {
+        // Read client message and receive index
+        if (extraMessageReader != null)
+        {
+            var stream = extraMessageReader.ReadMessage<IntegerMessage>();
+            curPlayer = stream.value;
+        }
+        //Select the prefab from the spawnable objects list
+        var playerPrefab = spawnPrefabs[curPlayer];
+
+        // Create player object with prefab
+        var player = Instantiate(playerPrefab, new Vector3(Random.Range(-2,2),0,0), Quaternion.identity) as GameObject;
+
+        // Add player object for connection
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+
+    public void btn0()
+    {
+        curPlayer = 0;
+    }
+
+    public void btn1()
+    {
+        curPlayer = 1;
+    }
+
+}
